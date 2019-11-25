d62b157ab8280093adff9e1f9665eb11013d6b1f
mahdi Lamriben <mehdilmbn@gmail.com>
2019-11-25 17:49:19 +0100

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+m = User.create(email: "mehdilmbn@gmail.com", password: "juxbar", username: "MadzMed", barman: true)
+p = User.create(email: "pierre@roger-roussel.com", password: "lenske", username: "Pierrot", barman: true)
+s = User.create(email: "simon@kobayashi-design.com", password: "juxbar", username: "Kobayashi", barman: true)
+f = User.create(email: "floriandbc@gmail.com", password: "juxbar", username: "floppy33300", barman: true)
+
+Bar.create(name: "Rockwood", address: "4 Quai Louis XVIII, 33000 Bordeaux", user_id: f.id)
+Bar.create(name: "Molly Malone's", address: "83 Quai des Chartrons, 33300 Bordeaux", user_id: m.id)
+Bar.create(name: "Mama Shelter", address: "19 Rue Poquelin Molière, 33000 Bordeaux", user_id: p.id)
+Bar.create(name: "Cafe Brun", address: "45 Rue Saint-Rémi, 33000 Bordeaux", user_id: s.id)
+Bar.create(name: "Le Wagon", address: "107 Cours Balguerie Stuttenberg, 33300 Bordeaux", user_id: m.id)
