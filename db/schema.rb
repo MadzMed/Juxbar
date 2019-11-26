869beb17c4b07a06972ef4da333a18bafc0f05f4
SimonLetellier <simon@kobayashi-design.com>
2019-11-26 19:00:27 +0100

    SessionShow2

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_11_26_131107) do
-
+ActiveRecord::Schema.define(version: 2019_11_26_170604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
