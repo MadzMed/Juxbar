be675153355910e0f9cbb4bb0c684ca1d96e0745
mahdi Lamriben <mehdilmbn@gmail.com>
2019-11-27 19:03:49 +0100

    playlists controller

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,7 +1,7 @@
 class PlaylistsController < ApplicationController
   def index
-    # @playlists = Playlist.where(session_id: params[:session_id])
-    @playlists = Playlist.all
+    @playlists = Playlist.where(session_id: params[:session_id])
+    # @playlists = Playlist.all
   end
 
   def update
