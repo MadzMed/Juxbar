bfe32e16d96d18987c0e18adf446d1d87f8c9ceb
Mahdi Lamriben <55115774+MadzMed@users.noreply.github.com>
2019-11-28 17:47:54 +0100

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,8 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
-import { heartLike } from '../plugins/heart_animation'
-import { playerAdvance } from '../plugins/player_wave'
+import { heartLike } from '../plugins/heart_animation';
+import { playerAdvance } from '../plugins/player_wave';
 
 initMapbox();
 heartLike();
