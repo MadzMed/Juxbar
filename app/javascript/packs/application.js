6c63984e9163c69c545f1989cdf32bd33c1cf3e5
floppy dublao <floriandbc@gmail.com>
2019-11-28 20:28:05 +0100

    night

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,36 +5,12 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { heartLike } from '../plugins/heart_animation';
 import { playerAdvance } from '../plugins/player_wave';
+import { song } from '../plugins/add_playlist';
 
 
 initMapbox();
 heartLike();
 playerAdvance();
-
-
-const song = () => {
-  const playlists = document.getElementById("top-playlist").children
-  for (let i=0; i<playlists.length - 1; i+=1) {
-    playlists[i].addEventListener("click", (e) => {
-      playlists[i].children["1"].children["0"].children["0"].classList.toggle("fa-plus");
-      playlists[i].children["1"].children["0"].children["0"].classList.toggle("fa-check");
-      console.dir(playlists[i].children["1"].children["0"].children["0"].classList);
-
-    //   const myPlaylist = document.getElementById("myplaylist");
-    //   console.log(myPlaylist)
-    //   console.dir(document.getElementById("first-card"));
-    //   if (document.getElementById("first-card") === null){
-    //     myPlaylist.style.display = "";
-    //     myPlaylist.insertAdjacentHTML("beforeend", "<div class='playlist container-playlist-card' style='z-index: 2; id='first-card'>patate</div>")
-    //   }else if (playlists[i].children["1"].children["0"].children["0"].classList["1"] === "fa-check") {
-    //     myPlaylist.insertAdjacentHTML("beforeend", "<div class='playlist container-playlist-card' style='z-index: 2;' id='first-card'>patate</div>")
-    //   } else {
-
-    //   }
-   });
-  };
-}
-
 song();
 
 
