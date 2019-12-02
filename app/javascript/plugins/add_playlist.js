ec05c5ebec1d8cd164b188383a9342d26b36c587
mahdi Lamriben <mehdilmbn@gmail.com>
2019-12-02 17:08:25 +0100

    add and delete song playlist

--- app/javascript/plugins/add_playlist.js
+++ app/javascript/plugins/add_playlist.js
@@ -7,20 +7,38 @@ const createPlaylist = () => {
       playlist.children["1"].children["0"].children["0"].classList.toggle("fa-plus");
       playlist.children["1"].children["0"].children["0"].classList.toggle("fa-check");
       // console.dir(playlists[i].children["1"].children["0"].children["0"].classList);
+      let newPlaylist = document.getElementById("first-card")
       let myPlaylist = document.getElementById("myplaylist");
       console.log(e.currentTarget)
       // console.log(playlist)
       // console.dir(playlist.children["1"])
       // console.dir(document.getElementById("myplaylist").children)
-      console.dir(document.getElementById("first-card").children)
-      if (document.getElementById("first-card").children.length === 0){
-        myPlaylist.style.display = "";
-        document.getElementById("first-card").insertAdjacentHTML("beforeend", `<div class="playlist-list-first d-flex justify-content-between children">${playlist.innerHTML}</div>`);
-      } else if (playlist.children["1"].children["0"].children["0"].classList["1"] === "fa-check") {
-        document.getElementById("first-card").insertAdjacentHTML("beforeend", `<div class="playlist-list d-flex justify-content-between children">${playlist.innerHTML}</div>`);
-      } else if (playlist.children["1"].children["0"].children["0"].classList["1"] === "fa-check") {
-
+      console.dir(newPlaylist.children)
+      if (newPlaylist.children.length < 5 ) {
+        if (newPlaylist.children.length === 0){
+          myPlaylist.style.display = "";
+          newPlaylist.insertAdjacentHTML("beforeend", `<div class="playlist-list-first d-flex justify-content-between children">${playlist.innerHTML}</div>`);
+          playlist.setAttribute('style', 'display:none !important')
+        } else if (playlist.children["1"].children["0"].children["0"].classList["1"] === "fa-check") {
+          newPlaylist.insertAdjacentHTML("beforeend", `<div class="playlist-list d-flex justify-content-between children">${playlist.innerHTML}</div>`);
+          playlist.setAttribute('style', 'display:none !important')
+        // } else if (playlist.children["1"].children["0"].children["0"].classList["1"] === "fa-check") {
+        }
+      } else {
+        alert("trop de son maggle")
       }
+      newPlaylist.querySelectorAll(".children").forEach((playlist) => {
+        if (newPlaylist.children.length === 1){
+          playlist.addEventListener("click", (e) => {
+            playlist.remove();
+            myPlaylist.style.display = "none";
+          });
+        } else if (newPlaylist.children.length > 0){
+          playlist.addEventListener("click", (e) => {
+            playlist.remove();
+          });
+        }
+      });
    });
   });
 }
