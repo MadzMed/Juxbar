d0f37e7335bb69f0600e78d30df419539bdf0c89
floppy dublao <floriandbc@gmail.com>
2019-12-02 21:34:59 +0100

    S2 lundi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import { heartLike } from '../plugins/heart_animation';
 import { playerAdvance } from '../plugins/player_wave';
 import { createPlaylist } from '../plugins/add_playlist';
 import { song } from '../plugins/add_playlist';
-<<<<<<< HEAD
 import { anim } from '../plugins/burger_animation_slider';
 import { home, pushEnter } from '../plugins/home';
 
