25aa671c101354895f7457842a24b024d3acb9d9
floppy dublao <floriandbc@gmail.com>
2019-12-02 17:10:28 +0100

    S2 lundi home+Seed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,14 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { heartLike } from '../plugins/heart_animation';
 import { playerAdvance } from '../plugins/player_wave';
 import { song } from '../plugins/add_playlist';
+import { home, pushEnter } from '../plugins/home';
 
 
 initMapbox();
 heartLike();
 playerAdvance();
 song();
+home();
+pushEnter();
 
 
