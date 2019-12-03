51aa09764046db12b17f948b3a2787cbcb9b1c35
mahdi Lamriben <mehdilmbn@gmail.com>
2019-12-03 14:22:12 +0100

    update index playlist AJAX

--- app/controllers/my/playlists_controller.rb
+++ app/controllers/my/playlists_controller.rb
@@ -8,7 +8,11 @@ class My::PlaylistsController < ApplicationController
   end
 
   def new
-    @playlist = Playlist.create(session_id: params[:session_id], user_id: 1, started_at: Time.now)
+    if current_user.playlists.find_by(session_id: params[:session_id])
+      @playlist = current_user.playlists.find_by(session_id: params[:session_id])
+    else
+      @playlist = Playlist.create(session_id: params[:session_id], user_id: current_user.id, started_at: Time.now)
+    end
     if params[:search].present?
       songs_serialized = open("https://api.deezer.com/search?q=#{params[:search][:query]}").read
       @songs = JSON.parse(songs_serialized)
@@ -16,36 +20,38 @@ class My::PlaylistsController < ApplicationController
       songs_serialized = open("https://api.deezer.com/chart/?limit=20").read
       @songs = JSON.parse(songs_serialized)
     end
+    if current_user.playlists.find_by(session_id: params[:session_id]).present?
+      @mysongs = current_user.playlists.find_by(session_id: params[:session_id]).songs
+    end
   end
 
   def create
     if user_signed_in? && current_user.playlists.present?
       if current_user.playlists.where(session_id: params[:session_id]).present?
         @playlist = current_user.playlists.find_by(session_id: params[:session_id])
-        @song = Song.new(songs_params)
-        @song.playlist_id = @playlist.id
-        @song.save
+        if (@playlist.songs.count < 5)
+          @song = Song.new(songs_params)
+          @song.playlist_id = @playlist.id
+          @song.save
+        end
       else
-        @playlist = Playlist.new(started_at: Time.now)
-        @playlist.user_id = current_user
-        @playlist.session_id = params[:session_id]
+        @playlist = Playlist.new(session_id: params[:session_id], user_id: current_user, started_at: Time.now)
         if @playlist.save
           @song = Song.new(songs_params)
           @song.playlist_id = @playlist.id
           @song.save
-          raise
         end
       end
-    else
-      @playlist = Playlist.find(params[:playlist_id])
-      @playlist.user_id =
-      @playlist.session_id = params[:session_id]
-      if @playlist.save
-        @song = Song.new(songs_params)
-        @song.playlist_id = @playlist.id
-        @song.save
-      end
-    end
+    # else
+    #   @playlist = Playlist.find(params[:playlist_id])
+    #   @playlist.user_id =
+    #   @playlist.session_id = params[:session_id]
+    #   if @playlist.save
+    #     @song = Song.new(songs_params)
+    #     @song.playlist_id = @playlist.id
+    #     @song.save
+    #   end
+     end
     if @playlist.save
       respond_to do |format|
         format.html { redirect_to my_playlist_path(params[:session_id]) }
@@ -67,6 +73,6 @@ class My::PlaylistsController < ApplicationController
   private
 
   def songs_params
-    params.require(:song).permit(:artist, :album, :category, :duration, :title, :deezer_id, :composer, :photo_album, :photo_large, :photo_artist)
+    params.require(:song).permit(:artist, :album, :category, :duration, :title, :deezer_id, :composer, :photo_album, :photo_large, :photo_artist, :preview)
   end
 end
