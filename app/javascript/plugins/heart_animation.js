b1efc9d7de2cbda7bd93fceafa2abf99954fd41e
mahdi Lamriben <mehdilmbn@gmail.com>
2019-12-04 02:16:31 +0100

    heart anim +

--- app/javascript/plugins/heart_animation.js
+++ app/javascript/plugins/heart_animation.js
@@ -1,18 +1,8 @@
 const heartLike = () => {
-  if (document.querySelectorAll(".fa-heart")) {
+  if (document.getElementById("heart")) {
     if (document.getElementById("heart")) {
       document.getElementById("heart").addEventListener("click", (e) => {
-        e.target.style.color = e.target.style.color === 'red' ? '' : 'red';
-      });
-    } else {
-      document.querySelectorAll(".fa-heart").forEach((element) => {
-        element.addEventListener("click", (e) => {
-          if (e.target.id !== "heart") {
-            e.target.classList.toggle("far");
-            e.target.classList.toggle("fas");
-            e.target.style.color = e.target.style.color === 'rgb(186, 27, 29)' ? '#262525' : 'rgb(186, 27, 29)';
-          }
-        });
+        e.target.style.color = e.target.style.color === 'rgb(186, 27, 29)' ? '' : 'rgb(186, 27, 29)';
       });
     }
   }
