2f48ec8a446dff89aa6418dcfdcffb3dc1258b9f
mahdi Lamriben <mehdilmbn@gmail.com>
2020-03-12 13:38:40 +0100

    delete comment

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,7 +1,6 @@
 class PlaylistsController < ApplicationController
   def index
     @playlists = Playlist.where(session_id: params[:session_id])
-    # @playlists = Playlist.all
   end
 
   def update
